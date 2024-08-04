df33cb21dea48f3ea1ea8149a9d5e5ae2d12eabe
matevzkusterle <matevz.kusterle@gmail.com>
2024-08-04 13:17:30 +0200

    popravil prikaz pacientov za admina in zdravnike + delno uredil dolžino kdoe

--- data/database.py
+++ data/database.py
@@ -1,6 +1,7 @@
 # uvozimo psycopg2
 import psycopg2, psycopg2.extensions, psycopg2.extras
-psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
+# se znebimo problemov s šumniki
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) 
 
 import os
 from typing import List, TypeVar, Type, Callable, Any
@@ -15,8 +16,9 @@ from datetime import date
 from dataclasses_json import dataclass_json
 
 import dataclasses
-# Ustvarimo generično TypeVar spremenljivko. Dovolimo le naše entitene, ki jih imamo tudi v bazi
-# kot njene vrednosti. Ko dodamo novo entiteno, jo moramo dodati tudi v to spremenljivko.
+# Ustvarimo generično TypeVar spremenljivko. Dovolimo le naše entitene, ki jih
+# imamo tudi v bazi kot njene vrednosti. Ko dodamo novo entiteno, jo moramo 
+# dodati tudi v to spremenljivko.
 
 
 DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
@@ -30,8 +32,9 @@ T = TypeVar(
     diagnoza,
     diagnoza_pacient,
     specializacije,
-    bridge
-
+    bridge,
+    spec_zdravnik
+    
     )
 
 class Repo:
@@ -39,18 +42,21 @@ class Repo:
     def __init__(self):
         
         # priklopimo se na bazo
-        self.conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=DB_PORT)
+        self.conn = psycopg2.connect(database=auth.db, host=auth.host, 
+                        user=auth.user, password=auth.password, port=DB_PORT)
         self.cur = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 
     def dobi_gen(self, typ: Type[T], take=10, skip=0) -> List[T]:
         """ 
-        Generična metoda, ki za podan vhodni dataclass vrne seznam teh objektov iz baze.
-        Predpostavljamo, da je tabeli ime natanko tako kot je ime posameznemu dataclassu.
+        Generična metoda, ki za podan vhodni dataclass vrne seznam teh objektov 
+        iz baze. Predpostavljamo, da je tabeli ime natanko tako kot je ime 
+        posameznemu dataclassu.
         """
 
 
-        # ustvarimo sql select stavek, kjer je ime tabele typ.__name__ oz. ime razreda
+        # ustvarimo sql select stavek, kjer je ime tabele typ.__name__ oz. 
+        # ime razreda
         tbl_name = typ.__name__
         
         sql_cmd = f'''SELECT * FROM {tbl_name} LIMIT {take} OFFSET {skip};'''
@@ -59,7 +65,8 @@ class Repo:
     
     def dobi_gen_id(self, typ: Type[T], id: int | str, id_col = "id") -> T:
         """
-        Generična metoda, ki vrne dataclass objekt pridobljen iz baze na podlagi njegovega idja.
+        Generična metoda, ki vrne dataclass objekt pridobljen iz baze na 
+        podlagi njegovega idja.
         """
         tbl_name = typ.__name__
         sql_cmd = f'SELECT * FROM {tbl_name} WHERE {id_col} = %s';
@@ -74,7 +81,8 @@ class Repo:
     
     def dobi_gen_dvoje(self, typ: Type[T], ime: str, priimek: str, prvi = "id", drugi = "id") -> T:
         """
-        Generična metoda, ki vrne dataclass objekt pridobljen iz baze na podlagi dveh podatkov.
+        Generična metoda, ki vrne dataclass objekt pridobljen iz baze na 
+        podlagi dveh podatkov.
         """
         tbl_name = typ.__name__
         sql_cmd = f'SELECT * FROM {tbl_name} WHERE {prvi} = %s AND {drugi} = %s';
@@ -87,7 +95,8 @@ class Repo:
     
         return typ.from_dict(d)
     
-    def izberi_paciente_zdravnika(self, ime: str, priimek: str) -> List[pacient]:
+    def izberi_paciente_zdravnika(self, ime: str, 
+                                  priimek: str) -> List[pacient]:
         """
         Vrne paciente danega zdravnika v seznamu.
         """
@@ -113,7 +122,8 @@ class Repo:
     
     def izbrisi_gen(self,  typ: Type[T], id: int | str, id_col = "id"):
         """
-        Generična metoda, ki vrne dataclass objekt pridobljen iz baze na podlagi njegovega idja.
+        Generična metoda, ki vrne dataclass objekt pridobljen iz baze na 
+        podlagi njegovega idja.
         """
         tbl_name = typ.__name__
         sql_cmd = f'Delete  FROM {tbl_name} WHERE {id_col} = %s';
@@ -125,9 +135,9 @@ class Repo:
     
     def dodaj_gen(self, typ: T, serial_col="id", auto_commit=True):
         """
-        Generična metoda, ki v bazo doda entiteto/objekt. V kolikor imamo definiram serial
-        stolpec, objektu to vrednost tudi nastavimo. Imena stolpev v bazi morajo biti enaka
-        imenom atributov objekta v modeli.py.
+        Generična metoda, ki v bazo doda entiteto/objekt. V kolikor imamo 
+        definiram serial stolpec, objektu to vrednost tudi nastavimo. Imena 
+        stolpev v bazi morajo biti enaka imenom atributov objekta v modeli.py.
         """
 
         tbl_name = type(typ).__name__
@@ -154,12 +164,13 @@ class Repo:
         if auto_commit: self.conn.commit()
 
         # Dobro se je zavedati, da tukaj sam dataclass dejansko
-        # "mutiramo" in ne ustvarimo nove reference. Return tukaj ni niti potreben.
+        # "mutiramo" in ne ustvarimo nove reference. Return tukaj ni niti 
+        # potreben.
       
     def dodaj_gen_list(self, typs: List[T], serial_col="id"):
         """
-        Generična metoda, ki v bazo zapiše seznam objekton/entitet. Uporabi funkcijo
-        dodaj_gen, le da ustvari samo en commit na koncu.
+        Generična metoda, ki v bazo zapiše seznam objekton/entitet. Uporabi 
+        funkcijo dodaj_gen, le da ustvari samo en commit na koncu.
         """
 
         if len(typs) == 0: return # nič za narest
@@ -196,8 +207,9 @@ class Repo:
 
     def posodobi_gen(self, typ: T, id_col = "id", auto_commit=True):
         """
-        Generična metoda, ki posodobi objekt v bazi. Predpostavljamo, da je ime pripadajoče tabele
-        enako imenu objekta, ter da so atributi objekta direktno vezani na ime stolpcev v tabeli.
+        Generična metoda, ki posodobi objekt v bazi. Predpostavljamo, da je ime
+        pripadajoče tabele enako imenu objekta, ter da so atributi objekta 
+        direktno vezani na ime stolpcev v tabeli.
         """
 
         tbl_name = type(typ).__name__
@@ -224,11 +236,13 @@ class Repo:
 
     def posodobi_list_gen(self, typs : List[T], id_col = "id"):
         """
-        Generična metoda, ki  posodobi seznam entitet(objektov). Uporabimo isti princip
-        kot pri posodobi_gen funkciji, le da spremembe commitamo samo enkrat na koncu.
+        Generična metoda, ki  posodobi seznam entitet(objektov). Uporabimo 
+        isti princip kot pri posodobi_gen funkciji, le da spremembe commitamo 
+        samo enkrat na koncu.
         """
         
-        # Posodobimo vsak element seznama, pri čemer sprememb ne comitamo takoj na bazi
+        # Posodobimo vsak element seznama, pri čemer sprememb ne comitamo 
+        # takoj na bazi
         for typ in typs:
             self.posodobi_gen(typ, id_col=id_col, auto_commit=False)
 
@@ -246,9 +260,10 @@ class Repo:
 
     def col_to_sql(self, col: str, col_type: str, use_camel_case=True, is_key=False):
         """
-        Funkcija ustvari del sql stavka za create table na podlagi njegovega imena
-        in (python) tipa. Dodatno ga lahko opremimo še z primary key omejitvijo
-        ali s serial lastnostjo. Z dodatnimi parametri, bi lahko dodali še dodatne lastnosti.
+        Funkcija ustvari del sql stavka za create table na podlagi njegovega 
+        imena in (python) tipa. Dodatno ga lahko opremimo še z primary key 
+        omejitvijo ali s serial lastnostjo. Z dodatnimi parametri, bi lahko 
+        dodali še dodatne lastnosti.
         """
 
         # ali stolpce pretvorimo v camel case zapis?
@@ -274,12 +289,15 @@ class Repo:
         # če ni ujemanj stolpec naredimo kar kot text
         return f'"{col}" TEXT{" PRIMARY KEY" if  is_key else ""}'
     
-    def df_to_sql_create(self, df: DataFrame, name: str, add_serial=False, use_camel_case=True) -> str:
+    def df_to_sql_create(self, df: DataFrame, name: str, add_serial=False, 
+                         use_camel_case=True) -> str:
         """
-        Funkcija ustvari in izvede sql stavek za create table na podlagi podanega pandas DataFrame-a. 
+        Funkcija ustvari in izvede sql stavek za create table na podlagi 
+        podanega pandas DataFrame-a. 
         df: DataFrame za katerega zgradimo sql stavek
         name: ime nastale tabele v bazi
-        add_serial: opcijski parameter, ki nam pove ali želimo dodat serial primary key stolpec
+        add_serial: opcijski parameter, ki nam pove ali želimo dodat serial 
+        primary key stolpec
         """
 
         # dobimo slovar stolpcev in njihovih tipov
@@ -307,9 +325,10 @@ class Repo:
 
     def df_to_sql_insert(self, df:DataFrame, name:str, use_camel_case=True):
         """
-        Vnese DataFrame v postgresql bazo. Paziti je treba pri velikosti dataframa,
-        saj je sql stavek omejen glede na dolžino. Če je dataframe prevelik, ga je potrebno naložit
-        po delih (recimo po 100 vrstic naenkrat), ali pa uporabit bulk_insert.
+        Vnese DataFrame v postgresql bazo. Paziti je treba pri velikosti 
+        dataframa, saj je sql stavek omejen glede na dolžino. Če je dataframe
+        prevelik, ga je potrebno naložit po delih (recimo po 100 vrstic
+        naenkrat), ali pa uporabit bulk_insert.
         df: DataFrame, ki ga želimo prenesti v bazo
         name: Ime tabele kamor želimo shranit podatke
         use_camel_case: ali pretovrimo stolpce v camel case zapis
@@ -354,8 +373,21 @@ class Repo:
 
         self.cur.execute(
             """
-            SELECT i.id, i.ime, i.priimek, i.szz, k.koda, k.detajli, k.aktivnost FROM pacient i
-            left join diagnoza k on i.id = k.id_pacient
+            SELECT 
+                k.id as id_diagnoza,
+                i.id,
+                i.ime, 
+                i.priimek, 
+                i.szz, 
+                k.koda, 
+                k.detajli, 
+                k.aktivnost 
+            FROM 
+                pacient i
+            LEFT JOIN 
+                diagnoza k 
+            ON 
+                i.id = k.id_pacient
             """)
         
         pacientt = self.cur.fetchall()
@@ -363,8 +395,10 @@ class Repo:
         if pacientt is None:
             return []
 
-        return [pacientDiag(id, ime, priimek, szz, koda, detajli, aktivnost) for \
-                (id, ime, priimek, szz, koda, detajli, aktivnost) in pacientt]
+        return [
+            pacientDiag(id_diagnoza, id, ime, priimek, szz, koda, detajli, aktivnost) for \
+                (id_diagnoza, id, ime, priimek, szz, koda, detajli, aktivnost) in pacientt
+                ]
     
     
     def pacient_to_pacientDiag(self, pacients: List[pacient]) -> List[pacientDiag]:
@@ -373,9 +407,23 @@ class Repo:
 
         self.cur.execute(
             f"""
-            SELECT i.id, i.ime, i.priimek, i.szz, k.koda, k.detajli, k.aktivnost FROM pacient i
-            LEFT JOIN diagnoza k ON i.id = k.id_pacient
-            WHERE i.id IN ({pacient_ids_str})
+            SELECT 
+                k.id as id_diagnoza,
+                i.id,
+                i.ime, 
+                i.priimek, 
+                i.szz, 
+                k.koda, 
+                k.detajli, 
+                k.aktivnost 
+            FROM 
+                pacient i
+            LEFT JOIN 
+                diagnoza k 
+            ON 
+                i.id = k.id_pacient
+            WHERE 
+                i.id IN ({pacient_ids_str})
             """
         )
 
@@ -384,8 +432,10 @@ class Repo:
         if pacientDiags is None:
             return []
 
-        return [pacientDiag(id, ime, priimek, szz, koda, detajli, aktivnost) for \
-                (id, ime, priimek, szz, koda, detajli, aktivnost) in pacientDiags]
+        return [
+            pacientDiag(id_diagnoza, id, ime, priimek, szz, koda, detajli, aktivnost) for \
+                (id_diagnoza, id, ime, priimek, szz, koda, detajli, aktivnost) in pacientDiags
+                ]
     
     def pacient(self) -> List[pacient]:
 
@@ -419,7 +469,17 @@ class Repo:
     def uporabnik(self) -> List[uporabnik]:
         self.cur.execute(
             """
-            SELECT i.username, i.id_zdravnik, i.id_pacient, i.role, i.ime, i.priimek, i.password_hash, i.last_login FROM uporabnik i
+            SELECT 
+                i.username, 
+                i.id_zdravnik, 
+                i.id_pacient, 
+                i.role, 
+                i.ime, 
+                i.priimek, 
+                i.password_hash, 
+                i.last_login 
+            FROM 
+                uporabnik i
             """)
         
         uporabnikk = self.cur.fetchall()
@@ -445,9 +505,20 @@ class Repo:
     def prikazi_diagnoze_pacienta(self, id: int) -> List[diagnoza]:
         self.cur.execute(
             """
-            SELECT i.koda, i.detajli, i.aktivnost, k.ime, k.priimek FROM diagnoza i
-            LEFT JOIN zdravnik k ON i.id_zdravnik = k.id
-            WHERE i.id_pacient = %s
+            SELECT 
+                i.koda, 
+                i.detajli, 
+                i.aktivnost, 
+                k.ime, 
+                k.priimek 
+            FROM 
+                diagnoza i
+            LEFT JOIN 
+                zdravnik k 
+            ON 
+                i.id_zdravnik = k.id
+            WHERE 
+                i.id_pacient = %s
             """
             , (id,))
         
